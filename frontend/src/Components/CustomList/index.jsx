b7c602ab1344cb77c76d07c45558d71bdcc36358
haydenanderl33 <hayden.anderl33@gmail.com>
2022-11-15 17:40:01 -0700

    1 done

--- frontend/src/Components/CustomList/index.jsx
+++ frontend/src/Components/CustomList/index.jsx
@@ -1,16 +1,65 @@
-import React from 'react';
-import { List, ListItem } from '@mui/material';
-import { ListItemDrawer } from '../ListItemDrawer';
+import React, { useEffect, useState } from "react";
+import { List, ListItem } from "@mui/material";
+import { ListItemDrawer } from "../ListItemDrawer";
 
 export function CustomList() {
-  const data = ['item', 'item', 'item', 'item'];
+  const [data, setData] = useState([]);
+  
+  let dataArr = [
+    {
+      name: "item 1",
+      viewed: false,
+      description: "item desciption",
+      status: ["new", "complete", "in progress", "on hold", "achieved"],
+    },
+    {
+      name: "item 2",
+      viewed: false,
+      description: "item desciption",
+      status: ["new", "complete", "in progress", "on hold", "achieved"],
+    },
+    {
+      name: "item 3",
+      viewed: false,
+      description: "item desciption 1",
+      status: ["new", "complete", "in progress", "on hold", "achieved"],
+    },
+    {
+      name: "item 4",
+      viewed: false,
+      description: "item desciption 1",
+      status: ["new", "complete", "in progress", "on hold", "achieved"],
+    },
+    {
+      name: "item 5",
+      viewed: false,
+      description: "item desciption 1",
+      status: ["new", "complete", "in progress", "on hold", "achieved"],
+    },
+  ];
+
+
+  const getData = () => {
+    setTimeout(() => {
+      setData([...dataArr])
+    }, 2000);
+  };
+
+  
+  useEffect(() => {
+    getData()
+  });
 
   return (
     <div>
-      <List>
-        {data.map((item) => (
-          <ListItem>{item}</ListItem>
-        ))}
+      <List style={{marginTop: '70px'}}>
+        {data.length > 0 ? (
+          data.map((item, index) => (
+            <ListItem key={index}>{item.name}</ListItem>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
       </List>
       <ListItemDrawer />
     </div>
